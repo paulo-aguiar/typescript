144e7e6eebe16074c9e9051cc36fa49e03baa1b5
paulo-aguiar <phaguiarrocha@gmail.com>
2022-07-18 20:43:11 -0300

    relação entre tipos de váriáveis

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var button = document.getElementById("button");
 var input1 = document.getElementById("input1");
 var input2 = document.getElementById("input2");
-function adicionarNumero(numero1, numero2) {
+function adicionarNumero(numero1, numero2, devePrintar, frase) {
+    var resultado = numero1 + numero2;
+    if (devePrintar) {
+        console.log(frase + resultado);
+    }
     return numero1 + numero2;
 }
+var devePrintar = true;
+var frase = "O valor é: ";
 if (button) {
     button.addEventListener('click', function () {
         if (input1 && input2) {
-            console.log(adicionarNumero(Number(input1.value), Number(input2.value)));
+            console.log(adicionarNumero(Number(input1.value), Number(input2.value), devePrintar, frase));
         }
     });
 }
