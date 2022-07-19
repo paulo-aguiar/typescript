f4408b0a92470d8a3dad9b6230e4a4d7ad1743dd
paulo-aguiar <phaguiarrocha@gmail.com>
2022-07-18 21:42:52 -0300

    tipos de variáveis personalizados

---
+++ exemplos/tipos-personalizados.ts
@@ -0,0 +1,15 @@
+//tipos personalizados
+type input = number | string;
+
+function somarValores(input1: input, input2: input) {
+  if (typeof input1 === "string" || typeof input2 === "string") {
+    return input1.toString() + input2.toString();
+  } else {
+    return input1 + input2;
+  }
+}
+
+console.log(somarValores(1, 6))
+console.log(somarValores('Olá, ', 'tudo bem?'))
+console.log(somarValores('Que dia é hoje? ', 6))
+
